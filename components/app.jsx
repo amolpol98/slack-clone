42250d448b16805e9523e3e5d0afceda13d4091a
amolpol_98 <amol.pol@toppr.com>
2016-03-05 23:25:59 +0530

    app jsx file changes

--- components/app.jsx
+++ components/app.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ChannelSection from './channels/channelsection.jsx';
 import UserSection from './users/usersection.jsx';
+import MessageSection from './messages/messagesection.jsx';
 
 class App extends Component {
 	constructor(props) {
@@ -8,7 +9,8 @@ class App extends Component {
 		this.state = {
 			channels: [],
 			activeChannel: {},
-			users: []
+			users: [],
+			messages: []
 		}
 	}
 	addChannel(name) {
@@ -24,6 +26,14 @@ class App extends Component {
 		users.push({id: users.length, name});
 		this.setState({users});
 	}
+	addMessage(body) {
+		let {activeChannel} = this.state;
+		let {messages, users} = this.state;
+		let createdAt = new Date;
+		let author = users.length > 0 ? users[0].name : 'anonymous';
+		messages.push({id: messages.length, body, createdAt, author});
+		this.setState({messages});
+	}
 	render() {
 		return (
 			<div className='app'>
@@ -38,7 +48,12 @@ class App extends Component {
 						{...this.state}
 						setUserName={this.setUserName.bind(this)}
 					/>
+			
 				</div>
+				<MessageSection
+					{...this.state}
+					addMessage={this.addMessage.bind(this)}
+				/>
 			</div>
 		)
 	}
